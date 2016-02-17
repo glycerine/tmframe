b960f3c0fde2a0fe75efce410cde9eae4c1bb2ff
Jason E. Aten <j.e.aten@gmail.com>
2016-02-16 18:08:52 -0800

    ft. for FirstInForceBefore

--- series.go
+++ series.go
@@ -38,6 +38,13 @@ func (s SearchStatus) String() string {
 	panic(fmt.Sprintf("unknown SearchStatus %v", s))
 }
 
+// FirstInForceBefore(), LastInForceBefore(),
+// FirstAtOrBefore(), and LastAtOrBefore()
+// provide searching functionality for through a
+// timeseries that may have duplicated timestamps.
+//
+// LastInForceBefore():
+//
 // If tm is greater than any seen Frame, InForceBefore()
 // will return the last seen Frame and a SearchStatus of InFuture.
 // If tm is smaller than the oldest Frame available,
@@ -47,16 +54,39 @@ func (s SearchStatus) String() string {
 // the TimeToPrimTm(tm) function. The 3rd return argument
 // is the integer index of the returned frame, or -1 if
 // SearchStatus is InPast.
-func (s *Series) InForceBefore(tm time.Time) (*Frame, SearchStatus, int) {
+func (s *Series) LastInForceBefore(tm time.Time) (*Frame, SearchStatus, int) {
 
 	m := len(s.Frames)
 	utm := TimeToPrimTm(tm)
-	// Search returns the smallest index i in [0, n) at which f(i) is true
+
+	// Search returns the smallest index i in [0, m) at which f(i) is true.
+	// If i == m, this means no such index had f(i) true.
 	i := sort.Search(m, func(i int) bool {
-		//Q("sort called at i=%v, returning %v b/c %v vs %v", i, s.Frames[i].Tm() >= utm, s.Frames[i].Tm(), utm)
 		return s.Frames[i].Tm() >= utm
 	})
-	//Q("i = %v", i)
+	if i == m {
+		return s.Frames[m-1], InFuture, m - 1
+	}
+
+	if i == 0 {
+		return nil, InPast, -1
+	}
+
+	return s.Frames[i-1], Avail, i - 1
+}
+
+// Like LastInForceBefore(), but if there are ties we return
+// the first in the sequence of tied timestamp Frames.
+func (s *Series) FirstInForceBefore(tm time.Time) (*Frame, SearchStatus, int) {
+
+	m := len(s.Frames)
+	utm := TimeToPrimTm(tm)
+
+	// Search returns the smallest index i in [0, m) at which f(i) is true.
+	// If i == m, this means no such index had f(i) true.
+	i := sort.Search(m, func(i int) bool {
+		return s.Frames[i].Tm() >= utm
+	})
 	if i == m {
 		return s.Frames[m-1], InFuture, m - 1
 	}
@@ -72,31 +102,26 @@ func (s *Series) FirstAtOrBefore(tm time.Time) (*Frame, SearchStatus, int) {
 
 	m := len(s.Frames)
 	utm := TimeToPrimTm(tm)
-	// Search returns the smallest index i in [0, n) at which f(i) is true.
-	// If i == n, this means no such index had f(i) true.
+
+	// Search returns the smallest index i in [0, m) at which f(i) is true.
+	// If i == m, this means no such index had f(i) true.
 	i := sort.Search(m, func(i int) bool {
-		//Q("FirstAtOrBefore sort called at i=%v, returning (%v >= %v) is %v", i, s.Frames[i].Tm(), utm, s.Frames[i].Tm() >= utm)
 		return s.Frames[i].Tm() >= utm
 	})
-	//Q("FirstAtOrBefore i = %v", i)
+
 	if i == m {
 		// all frames Tm < utm
 		rtm := s.Frames[m-1].Tm()
 
 		// Handling repeated timestamps:
-		// Spin back to the first Frame at rtm.
+		// Need to search back to the first Frame at rtm.
 		// For worst case efficiency of O(log(n)), rather
-		// than O(n), use Search() to
+		// than O(n), use Search() again to
 		// find the smallest index such that Tm == rtm.
 		k := sort.Search(m, func(i int) bool {
-			//Q("FirstAtOrBefore repeated equals search: sort called at i=%v, returning (%v == %v) is %v", i, s.Frames[i].Tm(), rtm, s.Frames[i].Tm() == rtm)
 			return s.Frames[i].Tm() == rtm
 		})
-		if k == m {
-			// no Frames had Tm <= rtm
-			//Q("no Frames had Tm <= rtm")
-			panic("this is impossible, rtm came from a Frame")
-		}
+		// k == m is impossible, rtm came from a Frame in s.Frames
 		return s.Frames[k], InFuture, k
 	}
 	// INVAR: at least one entry had Tm >= utm
@@ -119,20 +144,20 @@ func (s *Series) LastAtOrBefore(tm time.Time) (*Frame, SearchStatus, int) {
 
 	m := len(s.Frames)
 	utm := TimeToPrimTm(tm)
-	// Search returns the smallest index i in [0, n) at which f(i) is true.
-	// If i == n, this means no such index had f(i) true.
+
+	// Search returns the smallest index i in [0, m) at which f(i) is true.
+	// If i == m, this means no such index had f(i) true.
 	i := sort.Search(m, func(i int) bool {
-		//Q("LastAtOrBefore sort called at i=%v, returning %v b/c %v vs %v", i, s.Frames[i].Tm() >= utm, s.Frames[i].Tm(), utm)
 		return s.Frames[i].Tm() >= utm
 	})
-	//Q("LastAtOrBefore i = %v", i)
+
 	if i == m {
 		// all frames Tm < utm
 		return s.Frames[m-1], InFuture, m - 1
 	}
-	// INVAR: at least one entry had Tm >= utm
+	// INVAR: at least one entry had Tm >= utm.
 
-	// i is the smallest Frame such that itm >= utm,
+	// i is the smallest Frame such that itm >= utm.
 	itm := s.Frames[i].Tm()
 	if i == 0 {
 		if itm > utm {
@@ -148,16 +173,13 @@ func (s *Series) LastAtOrBefore(tm time.Time) (*Frame, SearchStatus, int) {
 	// find the smallest index such that Tm > itm,
 	// then subtract 1.
 	k := sort.Search(m, func(i int) bool {
-		//Q("LastAtOrBefore repeated equals search: sort called at i=%v, returning (%v == %v) is %v", i, s.Frames[i].Tm(), itm, s.Frames[i].Tm() > itm)
 		return s.Frames[i].Tm() > itm
 	})
 
 	if k == m {
-		//Q("no Frames had Tm > itm")
+		// no Frames had Tm > itm
 		return s.Frames[m-1], Avail, m - 1
 	}
-	if k == 0 {
-		panic("internal logic error, should be impossible since itm came from an existing Frame")
-	}
+	// k == 0 is impossible, since itm came from an existing Frame in s.Frames.
 	return s.Frames[k-1], Avail, k - 1
 }
--- series_test.go
+++ series_test.go
@@ -26,62 +26,97 @@ func Test011MovingAverage(t *testing.T) {
 
 func Test010InForceAtReturnsFrameBefore(t *testing.T) {
 
-	cv.Convey(`Given an Series s, the call s.InForceBefore(tm) should `+
-		`return the Frame strictly before tm, while AtOrBefore(tm) returns `+
-		`the first of (any repeated) precisely time matched tick if available at exactly tm`,
-		t, func() {
+	cv.Convey(`Given an Series s, the call s.LastInForceBefore(tm) should `+
+		`return the last Frame strictly before tm`, t, func() {
 
-			outpath := "test.frames.out.1"
-			testFrames, tms, by := GenTestFrames(5, &outpath)
+		outpath := "test.frames.out.1"
+		testFrames, tms, by := GenTestFrames(5, &outpath)
 
-			// chuck unmarshal of the generated frames while we're at it
-			rest := by
-			var err error
-			for i := range testFrames {
-				var newFr Frame
-				rest, err = newFr.Unmarshal(rest)
-				panicOn(err)
-				if !FramesEqual(&newFr, testFrames[i]) {
-					panic(fmt.Sprintf("frame %v error: expected '%s' to equal '%s' upon unmarshal, but did not.", i, newFr, testFrames[i]))
-				}
-			}
-
-			// read back from file, to emulate actual use.
-			f, err := os.Open(outpath)
+		// chuck unmarshal of the generated frames while we're at it
+		rest := by
+		var err error
+		for i := range testFrames {
+			var newFr Frame
+			rest, err = newFr.Unmarshal(rest)
 			panicOn(err)
-			fr := NewFrameReader(f, 1024*1024)
-
-			var frame Frame
-			i := 0
-			for ; err == nil; i++ {
-				_, _, err = fr.NextFrame(&frame)
-				if err != nil {
-					if err == io.EOF {
-						break
-					}
-					panic(fmt.Sprintf("tfcat error from fr.NextFrame() at i=%v: '%v'\n", i, err))
-				}
+			if !FramesEqual(&newFr, testFrames[i]) {
+				panic(fmt.Sprintf("frame %v error: expected '%s' to equal '%s' upon unmarshal, but did not.", i, newFr, testFrames[i]))
 			}
+		}
 
-			sers := NewSeriesFromFrames(testFrames)
-			at, status, i := sers.InForceBefore(tms[2])
-			//P("at, status = %v, %v", at, status)
-			cv.So(status, cv.ShouldEqual, Avail)
-			cv.So(time.Unix(0, at.Tm()).UTC(), cv.ShouldResemble, tms[1])
-			cv.So(i, cv.ShouldEqual, 1)
+		// read back from file, to emulate actual use.
+		f, err := os.Open(outpath)
+		panicOn(err)
+		fr := NewFrameReader(f, 1024*1024)
 
-			at, status, i = sers.InForceBefore(tms[4].Add(time.Hour))
-			//P("at, status = %v, %v", at, status)
-			cv.So(status, cv.ShouldEqual, InFuture)
-			cv.So(time.Unix(0, at.Tm()).UTC(), cv.ShouldResemble, tms[4])
-			cv.So(i, cv.ShouldEqual, 4)
+		var frame Frame
+		i := 0
+		for ; err == nil; i++ {
+			_, _, err = fr.NextFrame(&frame)
+			if err != nil {
+				if err == io.EOF {
+					break
+				}
+				panic(fmt.Sprintf("tfcat error from fr.NextFrame() at i=%v: '%v'\n", i, err))
+			}
+		}
 
-			at, status, i = sers.InForceBefore(tms[0])
-			//P("at, status = %v, %v", at, status)
-			cv.So(status, cv.ShouldEqual, InPast)
-			cv.So(at, cv.ShouldEqual, nil)
-			cv.So(i, cv.ShouldEqual, -1)
-		})
+		sers := NewSeriesFromFrames(testFrames)
+		at, status, i := sers.LastInForceBefore(tms[2])
+		//P("at, status = %v, %v", at, status)
+		cv.So(status, cv.ShouldEqual, Avail)
+		cv.So(time.Unix(0, at.Tm()).UTC(), cv.ShouldResemble, tms[1])
+		cv.So(i, cv.ShouldEqual, 1)
+
+		at, status, i = sers.LastInForceBefore(tms[4].Add(time.Hour))
+		//P("at, status = %v, %v", at, status)
+		cv.So(status, cv.ShouldEqual, InFuture)
+		cv.So(time.Unix(0, at.Tm()).UTC(), cv.ShouldResemble, tms[4])
+		cv.So(i, cv.ShouldEqual, 4)
+
+		at, status, i = sers.LastInForceBefore(tms[0])
+		//P("at, status = %v, %v", at, status)
+		cv.So(status, cv.ShouldEqual, InPast)
+		cv.So(at, cv.ShouldEqual, nil)
+		cv.So(i, cv.ShouldEqual, -1)
+	})
+
+	cv.Convey(`Given an Series ser, the call ser.FirstInForceBefore(t) should `+
+		`return the first Frame in any series of repeated frames tied at the`+
+		` same timestamp s, when s < t and no other timestamp r on an existing`+
+		` frame lives between them; there is no r: s < r < t`, t, func() {
+
+		repeat, tms, _ := GenTestFramesSequence(5, nil)
+
+		// have the middle 3 repeat the same time;
+		for i := 1; i < 4; i++ {
+			repeat[i].SetTm(repeat[3].Tm())
+		}
+
+		sers := NewSeriesFromFrames(repeat)
+
+		at, status, i := sers.FirstInForceBefore(tms[4])
+		cv.So(status, cv.ShouldEqual, Avail)
+		cv.So(at.GetV0(), cv.ShouldEqual, 1)
+		cv.So(i, cv.ShouldEqual, 1)
+
+		P("FristInForceBefore InFuture test")
+		at, status, i = sers.FirstInForceBefore(tms[4].Add(time.Hour))
+		cv.So(status, cv.ShouldEqual, InFuture)
+		cv.So(at.GetV0(), cv.ShouldEqual, 4)
+		cv.So(i, cv.ShouldEqual, 4)
+
+		sers.Frames = sers.Frames[:5]
+		at, status, i = sers.FirstInForceBefore(tms[4].Add(time.Hour))
+		cv.So(status, cv.ShouldEqual, InFuture)
+		cv.So(at.GetV0(), cv.ShouldEqual, 1)
+		cv.So(i, cv.ShouldEqual, 1)
+
+		at, status, i = sers.FirstInForceBefore(tms[0].Add(-time.Nanosecond))
+		cv.So(status, cv.ShouldEqual, InPast)
+		cv.So(at, cv.ShouldEqual, nil)
+		cv.So(i, cv.ShouldEqual, -1)
+	})
 
 	cv.Convey(`FirstAtOrBefore(tm) returns `+
 		`the first of (any repeated time timestamps) precisely time matched tick if available at exactly tm, `+
@@ -95,23 +130,6 @@ func Test010InForceAtReturnsFrameBefore(t *testing.T) {
 			// so we can distinguish if the first one was returned.
 			for i := 0; i < 5; i++ {
 				repeat[i].SetTm(repeat[0].Tm())
-			}
-
-			// read back from file, to emulate actual use.
-			f, err := os.Open(outpath)
-			panicOn(err)
-			fr := NewFrameReader(f, 1024*1024)
-
-			var frame Frame
-			i := 0
-			for ; err == nil; i++ {
-				_, _, err = fr.NextFrame(&frame)
-				if err != nil {
-					if err == io.EOF {
-						break
-					}
-					panic(fmt.Sprintf("tfcat error from fr.NextFrame() at i=%v: '%v'\n", i, err))
-				}
 			}
 
 			sers := NewSeriesFromFrames(repeat)
@@ -146,23 +164,6 @@ func Test010InForceAtReturnsFrameBefore(t *testing.T) {
 			// so we can distinguish if the first one was returned.
 			for i := 0; i < 5; i++ {
 				repeat[i].SetTm(repeat[0].Tm())
-			}
-
-			// read back from file, to emulate actual use.
-			f, err := os.Open(outpath)
-			panicOn(err)
-			fr := NewFrameReader(f, 1024*1024)
-
-			var frame Frame
-			i := 0
-			for ; err == nil; i++ {
-				_, _, err = fr.NextFrame(&frame)
-				if err != nil {
-					if err == io.EOF {
-						break
-					}
-					panic(fmt.Sprintf("tfcat error from fr.NextFrame() at i=%v: '%v'\n", i, err))
-				}
 			}
 
 			sers := NewSeriesFromFrames(repeat)
