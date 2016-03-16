556f590c4a5127605a1ed7765b921115937a296b
Jason E. Aten <j.e.aten@gmail.com>
2016-03-15 18:11:16 -0700

    grep multiple fields

--- cmd/tfgrep/tfgrep.go
+++ cmd/tfgrep/tfgrep.go
@@ -29,17 +29,20 @@ func main() {
 		showUse()
 		os.Exit(1)
 	}
-	field := leftover[0]
-	fmt.Fprintf(os.Stderr, "scanning-for-escaped-json-field='%s'\n", field)
-	re := regexp.MustCompile(fmt.Sprintf(`\\"%s\\":\\"([^\\]+)\\"`, field))
-	re2 := regexp.MustCompile(fmt.Sprintf(`"%s":([^,}]+)[,}]`, field))
+	are := make([]*regexp.Regexp, 0)
+	for i := range leftover {
+		field := leftover[i]
+		fmt.Fprintf(os.Stderr, "scanning-for-escaped-json-field='%s'\n", field)
+		re := regexp.MustCompile(fmt.Sprintf(`\\"%s\\":\\"([^\\]+)\\"`, field))
+		re2 := regexp.MustCompile(fmt.Sprintf(`"%s":([^,}]+)[,}]`, field))
+		are = append(are, re)
+		are = append(are, re2)
+	}
 
 	bufIn := bufio.NewReader(os.Stdin)
 	arr := make([]byte, 0, 1024*1024)
 	buf := bytes.NewBuffer(arr)
 	lineNum := int64(1)
-	matchCount := int64(0)
-	matchCount2 := int64(0)
 	for {
 		lastLine, err := bufIn.ReadBytes('\n')
 		if err != nil && err != io.EOF {
@@ -50,24 +53,19 @@ func main() {
 			break
 		}
 
-		match := re.FindAllSubmatch(lastLine, -1)
-		if match != nil {
-			if len(match[0]) > 1 {
-				for j := 1; j < len(match[0]); j++ {
-					matchCount++
-					fmt.Printf("%s\n", string(match[0][j]))
+		var s string
+		for _, re := range are {
+			match := re.FindAllSubmatch(lastLine, -1)
+			if match != nil {
+				if len(match[0]) > 1 {
+					for j := 1; j < len(match[0]); j++ {
+						s += string(match[0][j]) + " "
+					}
 				}
 			}
 		}
-
-		match2 := re2.FindAllSubmatch(lastLine, -1)
-		if match2 != nil {
-			if len(match2[0]) > 1 {
-				for j := 1; j < len(match2[0]); j++ {
-					matchCount2++
-					fmt.Printf("%s\n", string(match2[0][j]))
-				}
-			}
+		if len(s) > 0 {
+			fmt.Println(s)
 		}
 
 		buf.Reset()
@@ -78,5 +76,5 @@ func main() {
 		}
 	}
 
-	fmt.Fprintf(os.Stderr, "field='%s': found %v matches.\n", field, matchCount)
+	//fmt.Fprintf(os.Stderr, "field='%s': found %v matches.\n", field, matchCount)
 }
