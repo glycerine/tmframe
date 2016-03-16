32005fd7f8a20cdb42a9319c6d03d4d25bafb108
Jason E. Aten <j.e.aten@gmail.com>
2016-03-15 18:02:54 -0700

    tfgrep doesn't require slash-quotes

--- cmd/tfgrep/tfgrep.go
+++ cmd/tfgrep/tfgrep.go
@@ -32,12 +32,14 @@ func main() {
 	field := leftover[0]
 	fmt.Fprintf(os.Stderr, "scanning-for-escaped-json-field='%s'\n", field)
 	re := regexp.MustCompile(fmt.Sprintf(`\\"%s\\":\\"([^\\]+)\\"`, field))
+	re2 := regexp.MustCompile(fmt.Sprintf(`"%s":([^,}]+)[,}]`, field))
 
 	bufIn := bufio.NewReader(os.Stdin)
 	arr := make([]byte, 0, 1024*1024)
 	buf := bytes.NewBuffer(arr)
 	lineNum := int64(1)
 	matchCount := int64(0)
+	matchCount2 := int64(0)
 	for {
 		lastLine, err := bufIn.ReadBytes('\n')
 		if err != nil && err != io.EOF {
@@ -54,6 +56,16 @@ func main() {
 				for j := 1; j < len(match[0]); j++ {
 					matchCount++
 					fmt.Printf("%s\n", string(match[0][j]))
+				}
+			}
+		}
+
+		match2 := re2.FindAllSubmatch(lastLine, -1)
+		if match2 != nil {
+			if len(match2[0]) > 1 {
+				for j := 1; j < len(match2[0]); j++ {
+					matchCount2++
+					fmt.Printf("%s\n", string(match2[0][j]))
 				}
 			}
 		}
